ce1962eaf360e181decddf766046f0b2949226e3
Markus Meissner <markus.meissner@meissner.IT>
2015-02-10 20:35:52 +0100

    Updated git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,28 +4,75 @@ colors
 #export LSCOLORS="Gxfxcxdxbxegedabagacad"
 setopt prompt_subst
 
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}%{✚%G%}%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}%{✔%G%}%{$reset_color%}"
-# %{$fg_bold[green]%}
+# http://sebastiancelis.com/2009/11/16/zsh-prompt-git-users/
+# another nice prompt: https://github.com/olivierverdier/zsh-git-prompt
+update_current_git_vars() {
+    unset __CURRENT_GIT_BRANCH
+    unset __CURRENT_GIT_BRANCH_STATUS
+    unset __CURRENT_GIT_BRANCH_IS_DIRTY
 
-# show git branch/tag, or name-rev if on detached head
-parse_git_branch() {
-    (command git symbolic-ref -q HEAD || command git name-rev --name-only --no-undefined --always HEAD) 2>/dev/null
-}
+    local st="$(git status 2>/dev/null)"
+    if [[ -n "$st" ]]; then
+        local -a arr
+        arr=(${(f)st})
 
-# show red star if there are uncommitted changes
-parse_git_dirty() {
-    if command git diff-index --quiet HEAD 2> /dev/null; then
-      echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-    else
-      echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+        if [[ $arr[1] =~ 'Not currently on any branch.' ]]; then
+            __CURRENT_GIT_BRANCH='no-branch'
+        else
+            __CURRENT_GIT_BRANCH="${arr[1][(w)4]}";
+        fi
+
+        if [[ $arr[2] =~ 'Your branch is' ]]; then
+            if [[ $arr[2] =~ 'ahead' ]]; then
+                __CURRENT_GIT_BRANCH_STATUS='ahead'
+            elif [[ $arr[2] =~ 'diverged' ]]; then
+                __CURRENT_GIT_BRANCH_STATUS='diverged'
+            # with git v2 the sentence "Your branch is" is always printed, not only if it has changes
+            elif [[ $arr[2] =~ 'behind' ]]; then
+                __CURRENT_GIT_BRANCH_STATUS='behind'
+            fi
+        fi
+
+        if [[ ! $st =~ 'nothing to commit' ]]; then
+            __CURRENT_GIT_BRANCH_IS_DIRTY='1'
+        fi
     fi
 }
 
-# if in a git repo, show dirty indicator + git branch
 git_custom_status() {
-    local git_where="$(parse_git_branch)"
-    [ -n "$git_where" ] && echo " - [${git_where#(refs/heads/|tags/)}|$(parse_git_dirty)%{$reset_color%}]"
+    update_current_git_vars
+    if [ -n "$__CURRENT_GIT_BRANCH" ]; then
+        # git prompt
+        local gp=" - ["
+        # status
+        local s=""
+        gp+="$__CURRENT_GIT_BRANCH"
+        case "$__CURRENT_GIT_BRANCH_STATUS" in
+            ahead)
+            s+="↑"
+            ;;
+            diverged)
+            s+="↕"
+            ;;
+            behind)
+            s+="↓"
+            ;;
+        esac
+        if [ -n "$__CURRENT_GIT_BRANCH_IS_DIRTY" ]; then
+            #s+="⚡"
+            s+="%{✚%G%}"
+        fi
+        if [ -n "$s" ]; then
+            gp+=" %{$fg[red]%}"
+            gp+=$s;
+            gp+="%{$reset_color%}"
+        else
+            gp+=" %{$fg[green]%}%{✔%G%}%{$reset_color%}"
+        fi
+        gp+="]"
+
+        printf "%s" $gp
+    fi
 }
 
 if [[ $USER == "root" ]]; then
@@ -40,3 +87,4 @@ PS1='%{$fg[green]%}%m%{$reset_color%}:%{$fg[blue]%}%4c%{$reset_color%}$(git_cust
 %(!.#.$) '
 RPS1="%{$(echotc UP 1)%}(%D{%Y-%m-%d %H:%M})%{$(echotc DO 1)%}"
 #RPS1="$PR_BLUE(%D{%y-%m-%d %H:%M})$PR_NO_COLOR"
+# %{$fg_bold[green]%}
