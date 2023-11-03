40047e09c0f8bb85116a7fdfb6c39c4600cde760
Markus Meissner <markus.meissner@meissner.IT>
2023-11-03 16:05:06 +0100

    Added z configuration fix

---
+++ fish/conf.d/0-local.fish
@@ -0,0 +1,7 @@
+#echo 'LOAD "0-local.fish",8,1'
+
+# https://github.com/jethrokuan/z/issues/104
+set Z_DATA_DIR "$HOME/.local/share/z"
+set Z_DATA "$Z_DATA_DIR/data"
+set Z_EXCLUDE "^$HOME\$"
+
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
+    # https://fishshell.com/docs/current/index.html#configuration
+    # .fish scripts in ~/.config/fish/conf.d/ are also automatically executed before config.fish.
+    # -> see conf.d/0-local.fish for details
 
-    # https://github.com/jethrokuan/z/issues/104
-    set --universal Z_DATA ~/.local/share/z/data
-    set --universal Z_DATA_DIR ~/.local/share/z
+    set -gx PATH $PATH ~/bin
+    [ -d /opt/homebrew/bin ] && set -gx PATH $PATH /opt/homebrew/bin
 end
+
