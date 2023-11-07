d0a8d2edcbb3e4a692a4c8a14cc0aee847a13a33
Markus Meissner <markus.meissner@meissner.IT>
2023-11-07 23:02:51 +0100

    Added install-fzf

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,5 @@ if status is-interactive
 
     set -gx PATH $PATH ~/.local/bin ~/bin
     [ -d /opt/homebrew/bin ] && set -gx PATH $PATH /opt/homebrew/bin
-
-    if command -q lsb_release; and test "$(lsb_release --short --release)" -eq 10
-        # Don't set marker option in Debian 10
-        echo "Debian 10, setting FZF_DEFAULT_OPTS"
-        set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap'
-    end
 end
 
