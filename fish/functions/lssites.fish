d563a95337274b1021c839dd4f84286cc8330279
Markus Meissner <markus.meissner@meissner.IT>
2024-05-03 15:24:25 +0200

    Added /usr/local/etc/nginx/conf.d/ to lssites

--- fish/functions/lssites.fish
+++ fish/functions/lssites.fish
@@ -2,7 +2,12 @@ function lssites
     if test -n "$(find /etc -mindepth 2 -maxdepth 2 -name sites-enabled -type d 2>/dev/null)"
         set --append sites $(find /etc/*/sites-enabled/ -type l)
     end
-    set --append sites $(find /etc/nginx/conf.d/ -type f -name "server-*.conf")
+    if test -d /etc/nginx/conf.d
+        set --append sites $(find /etc/nginx/conf.d/ -type f -name "server-*.conf")
+    end
+    if test -d /usr/local/etc/nginx/conf.d
+        set --append sites $(find /usr/local/etc/nginx/conf.d/ -type f -name "server-*.conf")
+    end
     echo $sites | xargs ls -l
 end
 
