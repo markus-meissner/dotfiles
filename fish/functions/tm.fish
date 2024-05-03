b610fefb69db1adf295aba02b65ccae2da848d82
Markus Meissner <markus.meissner@meissner.IT>
2024-05-03 16:59:29 +0200

    Added $argv to tm

--- fish/functions/tm.fish
+++ fish/functions/tm.fish
@@ -1,15 +1,15 @@
 function tm -d "tail the system logs"
     switch (uname | string lower)
         case darwin
-            sudo tail -F /var/log/system.log /var/log/secure.log
+            sudo tail -F /var/log/system.log /var/log/secure.log $argv
         case freebsd openbsd dragonfly
-            sudo tail -F /var/log/auth.log /var/log/cron /var/log/security /var/log/messages
+            sudo tail -F /var/log/{auth.log,cron,daemon.log,security,messages} $argv
         case linux
             switch (_detect_linux_flavor | string lower)
                 case redhat
-                    sudo tail -F /var/log/secure /var/log/cron /var/log/messages
+                    sudo tail -F /var/log/secure /var/log/cron /var/log/messages $argv
                 case '*'
-                    sudo tail -F /var/log/auth.log /var/log/syslog
+                    sudo tail -F /var/log/auth.log /var/log/syslog $argv
             end
         case '*'
             echo -ns '?'
