5319f27c692ffd4abac61ac12ca355be2987b841
Markus Meissner <markus.meissner@meissner.IT>
2025-05-04 22:42:54 +0200

    Activated nvim-lint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -6,7 +6,8 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- https://github.com/DavidAnson/markdownlint/blob/main/README.md#configuration
+        markdown = { 'markdownlint-cli2' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
