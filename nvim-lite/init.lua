c57ccc055f1633b0ed2c042a9e0f39940b39a983
Markus Meissner <markus.meissner@meissner.IT>
2026-04-05 22:10:53 +0200

    Added vim.pack to nvim-lite

--- nvim-lite/init.lua
+++ nvim-lite/init.lua
@@ -12,5 +12,33 @@ require('options')
 require('keymaps')
 
 -- require("config.lazy")
+if vim.fn.has 'nvim-0.12' == 1 then
+  vim.pack.add({
+    "https://github.com/gbprod/cutlass.nvim",
+    "https://github.com/nvim-mini/mini.move",
+  })
+
+  require('cutlass').setup {
+    cut_key = 'm',
+  }
+
+  require('mini.move').setup {
+    -- Module mappings. Use `''` (empty string) to disable one.
+    -- 2025-03-30: Left and right are not working for unknown reasons
+    mappings = {
+      -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+      left = '<C-Left>',
+      right = '<C-Right>',
+      down = '<C-Down>',
+      up = '<C-Up>',
+
+      -- Move current line in Normal mode
+      line_left = '<C-Left>',
+      line_right = '<C-Right>',
+      line_down = '<C-Down>',
+      line_up = '<C-Up>',
+    },
+  }
+end
 
 -- vim: ts=2 sts=2 sw=2 et
