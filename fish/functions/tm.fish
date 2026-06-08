1d3a9291bad25608c1f35f673370a1b4a701a03a
Markus Meissner <markus.meissner@meissner.IT>
2026-06-08 06:23:54 +0200

    Upgraded tm / tml

--- fish/functions/tm.fish
+++ fish/functions/tm.fish
@@ -1,20 +1,31 @@
 function tm -d "tail the system logs"
+    # In previous versions $argv was appended to the command in order to tail
+    # more files, e.g.
+    # tm /var/log/zabbix/zabbix-agent.log
+    # This leads to the problem that
+    # tm | grep my-string
+    # does not work.
+    # → Perhaps abbr is a better solution.
     switch (uname | string lower)
         case darwin
-            sudo tail -F /var/log/system.log /var/log/secure.log $argv
+            sudo tail -F /var/log/system.log /var/log/secure.log
         case dragonfly freebsd
-            sudo tail -F /var/log/{auth.log,cron,daemon.log,security,messages} $argv
+            sudo tail -F /var/log/{auth.log,cron,daemon.log,security,messages}
         case openbsd
-            sudo tail -f /var/log/{authlog,daemon,maillog,messages,secure} $argv
+            sudo tail -f /var/log/{authlog,daemon,maillog,messages,secure}
         case linux
             switch (_detect_linux_flavor | string lower)
                 case redhat
-                    sudo tail -F /var/log/secure /var/log/cron /var/log/messages $argv
+                    sudo tail -F /var/log/secure /var/log/cron /var/log/messages
                 case '*'
-                    sudo tail -F /var/log/auth.log /var/log/syslog $argv
+                    if test -f /var/log/auth.log
+                        sudo tail -F /var/log/auth.log /var/log/syslog
+                    else
+                        echo "Using journalctl"
+                        journalctl -f
+                    end
             end
         case '*'
             echo -ns '?'
     end
 end
-
--- fish/functions/tml.fish
+++ fish/functions/tml.fish
@@ -1,4 +1,8 @@
-function tml
-    sudo tail -F $(sudo find /var/log -name dovecot.log -o -name maillog -o -name mail.log)
+function tml -d "tail mail logs"
+    switch (uname | string lower)
+        case openbsd
+            tail -f /var/log/maillog
+        case '*'
+            sudo tail -F $(sudo find /var/log -name dovecot.log -o -name maillog -o -name mail.log)
+    end
 end
-    
